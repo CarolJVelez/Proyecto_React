fcffeb9d1be1e165c1db332cf5ded4113d627b9a
carol velez <carol.velez@supergiros.com.co>
2024-05-18 00:08:31 -0500

    Creacion de variables

---
+++ github-user-app/src/GitHubUser.js
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+
+function GitHubUser() {
+  //Declaracion de variables
+  const [nombreUsuario, setUsername] = useState('');//usuarioGithub
+  const [datosUsuario, setUserData] = useState(null);//datos del usuarioGithub
+  const [repositorio, setRepos] = useState([]);//lista
+
+}
+  export default GitHubUser;
